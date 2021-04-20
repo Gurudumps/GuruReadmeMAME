6be176aad1ad57e6bfaabd370364c08fab58db63
Gurudumps <mr.t.guru@gmail.com>
2021-04-20 19:16:52 +0800

    Create 3x3puzzl.cpp

---
+++ src/mame/drivers/misc/3x3puzzl.cpp
@@ -0,0 +1,19 @@
+/***************************************************************************
+
+3x3 Puzzle, Ace Enterprise
+Hardware info by Guru
+Last Updated: No changes
+---------------------
+Typical and simple Korean hardware.....
+
+68000 @ 10MHz
+OKI M6295 @ 1MHz (4/4)
+Lattice pLSI1032 for graphics generation
+6264 SRAM x 2
+6116 SRAM x 8
+62256 SRAM x2 (main program RAM)
+DIP SW 8-position X2 (SW1 SW2)
+OSC 27MHz, 22.1184MHz, 10MHz
+XTAL 4MHz
+
+***************************************************************************/
