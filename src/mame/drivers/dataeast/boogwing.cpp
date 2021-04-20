2cbf92f5ea866e2cc2949652f1204514ca1e335d
Gurudumps <mr.t.guru@gmail.com>
2021-04-21 00:17:53 +0800

    Add files via upload

---
+++ src/mame/drivers/dataeast/boogwing.cpp
@@ -0,0 +1,55 @@
+/***************************************************************************
+
+Boogie Wings (aka The Great Ragtime Show), Data East, 1992
+Hardware info by Guru
+Last updated: No changes
+------------------------
+
+PCB No: DE-0379-1
+
+CPU: DE102
+Sound: HuC6280A, YM2151, YM3012, OKI M6295 (x2)
+OSC: 32.220MHz, 28.000MHz
+DIPs: 8 position (x2)
+PALs: (not dumped) VF-00 (PAL16L8)
+                   VF-01 (PAL16L8)
+                   VF-02 (22CV10P)
+                   VF-03 (PAL16L8)
+                   VF-04 (PAL16L8)
+                   VF-05 (PAL16L8)
+
+PROM: MB7122 (compatible with 82S137, located near MBD-09)
+
+RAM: 62256 (x2), 6264 (x5)
+
+Data East Chips:52 (x2)
+                141 (x2)
+                102
+                104
+                113
+                99 (ACE CHIP)
+                71 (x2)
+                200
+
+ROMs:
+kn00-2.2b   27c2001   \
+kn01-2.4b   27c2001    |  Main program
+kn02-2.2e   27c2001    |
+kn03-2.4e   27c2001   /
+kn04.8e     27c512    \
+kn05.9e     27c512    /   near 141's and MBD-00, MBD-01 and MBD-02
+kn06.18p    27c512        Sound Program
+mbd-00.8b   16M
+mbd-01.9b   16M
+mbd-02.10e  4M
+mbd-03.13b  16M
+mbd-04.14b  16M
+mbd-05.16b  16M
+mbd-06.17b  16M
+mbd-07.18b  16M
+mbd-08.19b  16M
+mbd-09.16p  4M         Oki Samples
+mbd-10.17p  4M         Oki Samples
+
+***************************************************************************/
+
