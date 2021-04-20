948862fff032704109eb1c274ca53caeb4b0f695
Gurudumps <mr.t.guru@gmail.com>
2021-04-20 23:56:54 +0800

    Add files via upload

---
+++ src/mame/drivers/misc/blmbycar.cpp
@@ -0,0 +1,14 @@
+/***************************************************************************
+
+Blomby Car, Abm & Gecas, 1990
+Hardware info by Guru
+Last updated: No changes
+------------------------
+
+CPU : MC68000P12
+SND : Oki M6295 (samples only)
+OSC : 30.000MHz, 24.000MHz & 1.00MHz resonator
+DSW : 2x 8-position
+GFX : TI TPC1020AFN-084
+
+***************************************************************************/
