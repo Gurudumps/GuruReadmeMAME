a3179d9df8dde976a7913df3eda750358f782ef1
Gurudumps <mr.t.guru@gmail.com>
2021-04-20 20:15:06 +0800

    Create airraid.cpp

---
+++ src/mame/drivers/seibu/airraid.cpp
@@ -0,0 +1,32 @@
+/**************************************************************************
+
+Cross Shooter, J K H Corp (Seibu 1987)
+Hardware info by Guru
+Last updated: No changes
+------------------------
+
+Seibu Hardware
+PCB is coloured black
+
+PCB No. S-0087-011A-0
+CPU: SHARP LH0080B (Z80B)
+SND: YM2151, Z80A, SEI80BU 611 787, YM3012, SEI0100BU YM3931
+RAM: TMM2015 x 7, TMM2063 x 1
+DIPs: 2 x 8 position
+CMOS Gate Arrays: SEI0020BU TC17G008AN-0015 (x 3), SEI10040BU TC15G008AP-0048,
+                  SEI0030BU TC17G005AN-0026, SEI0060BU TC17G008AN-0024
+OTHER: SEI0050BU M  6 4 0 00
+XTAL: 14.318 MHz (near SEI80BU), xx.000 MHz (cant read speed, near SEI0040BU)
+
+There are 3 BIG custom black packs on the PCB.
+
+ROMS:
+Note, all ROMs have official sticker, "(C) SEIBU KAIHATSU INC." and a number.
+
+1.k19  TMM27256      \
+2.k20  TMM27512      / Program
+3.f11  TMM2764         Gfx?
+4.g8   TMM24256 Mask   Sound (Samples?)
+5.g6   TMM2764         Sound program
+
+**************************************************************************/
